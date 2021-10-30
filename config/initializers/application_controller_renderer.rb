fb2a8e5465e26ad8f91c0ef5062cfa316c71a664
not1214 <91748221+not1214@users.noreply.github.com>
2021-10-30 22:02:18 +0900

    Update image

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '54fdc7948e9a84f1bd857b0a8cd9bffb015b13106d5964159e96b13c2ce69e29aa2b367c5b222bdd54bbacd42d3fba433b556af169b4737271f430349ccacfdb'
